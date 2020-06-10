8d8fc5b5d5ed0ab2b5fba544c0a03b50f563d3d8
haotian <haotian.chen@live.com>
2020-06-09 23:29:41 -0400

    business and manager costs working

---
+++ AdventureCapitalist/assets/stylesheets/buttonStyles.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const button = {
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: 'black',
+    margin: 10,
+    backgroundColor: 'orange',
+    justifyContent: 'center',
+    alignItems: 'center'
+};
+
+const buttonText = {
+    fontSize: 18,
+    color: 'white'
+}
+
+export default {
+    button: button,
+    buttonText: buttonText
+};
+
+
+
+
+
